2223917cd9888efb23134abc2fc3d1ee0beff5c8
Marwanthe0 <marwanthe0@gmail.com>
2025-02-04 21:59:38 +0600

    Add ATM_Booth.java

---
+++ Second Year/2_1/Object Oriented Programming/Lab/ATM_MACHINE/ATM_Booth.java
@@ -0,0 +1,12 @@
+package ATM_MACHINE;
+import java.util.*;
+
+// Main class to start the ATM simulation
+public class ATM_Booth{
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        atm.start();
+//        System.out.println("ATM_Booth");
+    }
+}
+
