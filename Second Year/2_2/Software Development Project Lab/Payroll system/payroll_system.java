8365827f54e6b78eb85a174e79bd434503f6718c
Marwanthe0 <marwanthe0@gmail.com>
2025-04-22 11:39:59 +0600

    Add payroll_system.java

---
+++ Second Year/2_2/Software Development Project Lab/Payroll system/payroll_system.java
@@ -0,0 +1,41 @@
+class Employee {
+    String name;
+    double salary;
+    int age;
+}
+
+class Teacher extends Employee {
+    int id;
+    String rank;
+
+    Teacher(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+class Officer extends Employee {
+    int id;
+    String rank;
+
+}
+
+class payroll_system {
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        Teacher t = new Teacher(20, "Rayhan", 250000);
+        System.out.println("Teacher name= " + t.name);
+        System.out.println("Teacher id = " + t.id);
+        System.out.println("Teacher salary= " + t.salary);
+
+        Officer mamun = new Officer();
+        mamun.name = "Mamun";
+        mamun.rank = "Sub-Assistance Engineer";
+        mamun.salary = 2000;
+        mamun.age = 29;
+        System.out.println("Officer name= " + mamun.name);
+        System.out.println("Officer id = " + mamun.id);
+        System.out.println("Officer salary= " + mamun.salary);
+    }
+}
