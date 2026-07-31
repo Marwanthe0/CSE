20b16efba5bea481dbdbf3d85029d41f0cfec66d
Marwanthe0 <marwanthe0@gmail.com>
2026-08-01 01:48:03 +0600

    Add/Update CreateProductDTO.cs

--- Third Year/3-2/Software Engineering Project/WebApp1/DTOs/CreateProductDTO.cs
+++ Third Year/3-2/Software Engineering Project/WebApp1/DTOs/CreateProductDTO.cs
@@ -1,8 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+
 namespace WebApp1;
 
 public record CreateProductDTO(
-    [Required][StringLength(50)] string Name,
-    [Required][StringLength(20)] string Category,
+    [Required] [StringLength(50)] string Name,
+    [Range(1, 50)] int CategoryId,
     DateOnly ReleaseDate
 );
