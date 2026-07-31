5d50290b678e10fc09f206022aa2708d0f9343e4
Marwanthe0 <marwanthe0@gmail.com>
2026-08-01 03:32:51 +0600

    Add/Update Program.cs

--- Third Year/3-2/Software Engineering Project/WebApp1/Program.cs
+++ Third Year/3-2/Software Engineering Project/WebApp1/Program.cs
@@ -2,15 +2,16 @@ using DTOs;
 using WebApp1;
 using WebApp1.Data;
 using WebApp1.EndPoints;
+using WebApp1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
 //Adding Validation service
 builder.Services.AddValidation();
-
 //Connecting WebApp1.db database with context
-var connString = "Data Source=WebApp1.db";
-builder.Services.AddSqlite<WebApp1Context>(connString);
-
+builder.AddWebapp1Db();
 var app = builder.Build();
 app.MapProductsEndPoints();
+app.MapCategoriesEndPoints();
+app.MigrateDb();
 app.Run();
