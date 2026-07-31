81df9dd70eee8a12282f1ed0f607b929b000782a
Marwanthe0 <marwanthe0@gmail.com>
2026-08-01 03:18:49 +0600

    Add/Update UpdateProductDTO.cs

--- Third Year/3-2/Software Engineering Project/WebApp1/DTOs/UpdateProductDTO.cs
+++ Third Year/3-2/Software Engineering Project/WebApp1/DTOs/UpdateProductDTO.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace WebApp1;
 
 public record UpdateProductDTO(
-    [Required][StringLength(50)] string Name,
-    [Required][StringLength(20)] string Category,
+    [Required] [StringLength(50)] string Name,
+    [Range(1, 50)] int CategoryId,
     DateOnly ReleaseDate
 );
