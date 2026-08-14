a6ffa90a767f36a208236b624f20591e19a975c6
Marwanthe0 <marwanthe0@gmail.com>
2026-08-15 02:01:06 +0600

    Add Customer repository implementation and interface for data access

--- Third Year/3-2/Software Engineering Project/HotelManagement/src/HotelManagement.Domain/Entities/Customer.cs
+++ Third Year/3-2/Software Engineering Project/HotelManagement/src/HotelManagement.Domain/Entities/Customer.cs
@@ -7,5 +7,9 @@ public class Customer
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set;} = string.Empty;
     public string Phone { get; set;} = string.Empty;
-    public string Address { get; set;} = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    
+    // Navigation Property
+    public ICollection<Booking> Bookings { get; set; }
+        = new List<Booking>();
 }
