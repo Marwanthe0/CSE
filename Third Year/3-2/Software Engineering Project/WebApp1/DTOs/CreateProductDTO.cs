18fa3d5ff673d791f2a3e77ec883e10f479b9285
Marwanthe0 <marwanthe0@gmail.com>
2026-08-17 01:41:52 +0600

    Delete CreateProductDTO.cs

--- Third Year/3-2/Software Engineering Project/WebApp1/DTOs/CreateProductDTO.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace WebApp1;
-
-public record CreateProductDTO(
-    [Required] [StringLength(50)] string Name,
-    [Range(1, 50)] int CategoryId,
-    DateOnly ReleaseDate
-);
