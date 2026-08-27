0c31efb64d197dad02c4c406807599adca3317e7
Marwanthe0 <marwanthe0@gmail.com>
2026-08-27 16:11:36 +0600

    Add UpdateEmployeeDTO.cs

---
+++ Third Year/3-2/Software Engineering/HotelManagement/src/HotelManagement.Application/DTOs/Employees/UpdateEmployeeDTO.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelManagement.Application.DTOs.Employees;
+
+public class UpdateEmployeeDTO
+{
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(100)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    public string Phone { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(50)]
+    public string Role { get; set; } = string.Empty;
+
+    [Range(0, 10000000)]
+    public decimal Salary { get; set; }
+}
